ad06b30fdae213bd6b510a316ff71d45821ee015
Fabrício <guilhermo.fabricio@gmail.com>
2020-06-09 14:15:27 -0300

    Initial commit

---
+++ login/login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by Fabrício Guilhermo on 09/06/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
